5d4141b8640449fa2589dfc69f063b6202fcf96d
Vlad Keliy <limonkeliy153@gmail.com>
2019-12-22 22:49:48 +0300

    :tada: Add timestamp microservice

---
+++ src/main.ts
@@ -0,0 +1,56 @@
+import express from 'express';
+
+const app = express();
+
+const INVALID_DATE = 'Invalid Date';
+
+interface Timestamp {
+  unix: number;
+  utc: string;
+}
+
+interface TimestampError {
+  error: string;
+}
+
+const prepareDateResponse = (date: Date): Timestamp => ({
+  unix: date.getTime(),
+  utc: date.toUTCString(),
+});
+
+const getTimestampObject = (
+  dateString: string | undefined,
+): Timestamp | TimestampError => {
+  if (!dateString) {
+    const date = new Date();
+    return prepareDateResponse(date);
+  }
+
+  try {
+    const unixDate = parseInt(dateString);
+    const date = new Date(isNaN(unixDate) ? dateString : unixDate);
+
+    if (date.toString() === INVALID_DATE) {
+      throw new Error(INVALID_DATE);
+    }
+
+    return prepareDateResponse(date);
+  } catch (e) {
+    return { error: e.message };
+  }
+};
+
+app.get('/api/timestamp/:date_string?', (req, res) => {
+  const dateString = req.params.date_string;
+
+  const responseObj = getTimestampObject(dateString);
+  const isError = 'error' in responseObj;
+
+  if (isError) {
+    res.statusCode = 400;
+  }
+
+  res.json(responseObj);
+});
+
+app.listen(3000);
